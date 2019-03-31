ba507c93c237045a6c6556e65590dc35eaea0b99
namiq_bay <virtualzombi@gmail.com>
2019-03-31 11:29:36 -0700

    Rail Fence Encrypt Algorithm added

---
+++ src/examples/RailFence.java
@@ -0,0 +1,35 @@
+package examples;
+
+public class RailFence {
+
+	public static void main(String[] args) {
+		
+		String plainText = "helloworld";
+		int k = 4;
+		int a = 0;
+		int l = plainText.length();
+		char cryptArr[][] = new char[k][l];
+		boolean bool = false;
+		
+		for (int i = 0; i < l; i++) {
+			cryptArr[a][i] = plainText.charAt(i);
+
+			if (a == k - 1)
+				bool = true;
+			if (a == 0)
+				bool = false;
+
+			if (bool)
+				a--;
+			else
+				a++;
+		}
+		
+		for (char ch[] : cryptArr) {
+			for (char i : ch) {
+				if (i != 0)
+					System.out.print(i);
+			}
+		}
+	}
+}
